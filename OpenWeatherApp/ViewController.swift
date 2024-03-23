dd50b35766b2108c49cfe601dd2261fa3a405d9b
Kirill <K-baranov@outlook.com>
2024-03-23 22:07:13 +0100

    Initial Commit

---
+++ OpenWeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenWeatherApp
+//
+//  Created by Kirill Baranov on 23/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
